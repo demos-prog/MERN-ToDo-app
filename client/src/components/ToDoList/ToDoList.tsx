eb8100cc3a08ab04af1b3d35e59eac2b5157171c
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-05-28 13:57:50 +0300

    req

--- client/src/components/ToDoList/ToDoList.tsx
+++ client/src/components/ToDoList/ToDoList.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+type ToDo = {
+  text: string,
+  complition: boolean,
+}
+
+type User = {
+  name: string,
+  password: string,
+  todos: ToDo[]
+}
 
 const ToDoList: React.FC = () => {
   const location = useLocation();
-
+  const [user, setuser] = useState<User>()
 
   const name = location.pathname.split('/').pop()
 
+  const getUsersData = useCallback(async () => {
+    const response = await fetch(`http://localhost:5050/todo/${name}`);
+    return await response.json();
+  }, [name]);
+
+  useEffect(() => {
+    getUsersData().then((data) => {
+      console.log(data);
+      
+      setuser(data)
+    })
+  }, [getUsersData])
 
   return (
     <div>
