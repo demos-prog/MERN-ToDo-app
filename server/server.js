bf981d41b649e56af129873f6298a94804cbf92f
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-06-07 12:26:33 +0300

    build

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import todo from './routes/todo.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 dotenv.config();
 
@@ -12,8 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/todo", todo);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/index.htm'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}/`);
