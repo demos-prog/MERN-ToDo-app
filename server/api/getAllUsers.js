1e63ff7599e30df22c7bfd79f5c475147b0483fc
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-06-25 09:34:40 +0300

    send

--- server/api/getAllUsers.js
+++ server/api/getAllUsers.js
@@ -1,13 +1,13 @@
 // server/api/getAllUsers.js
 import db from "../db/todousers.js";
 
-export default async function handler(req, res) {
+export default async function handler(_, res) {
   try {
     let collection = db.collection("todousers");
     let results = await collection.find({}).toArray();
-    res.status(200).json(results);
+    res.status(200).send(results);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Error fetching all users" });
+    res.status(500).send({ message: "Error fetching all users" });
   }
 }
