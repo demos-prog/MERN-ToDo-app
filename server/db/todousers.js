3c3447823b70a3e63a0d9dc050c61179438c125f
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-06-25 09:45:03 +0300

    uri

--- server/db/todousers.js
+++ server/db/todousers.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const uri = process.env.ATLAS_URI || "";
+// const uri = process.env.ATLAS_URI || "";
+const uri = 'mongodb+srv://demos:3465@cluster0.ixp4znt.mongodb.net/';
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
